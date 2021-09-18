527f83b7ca3d8129d745485207baa458bbfc8fda
innazozulia <innazozulia@outlook.com>
2021-09-18 22:47:41 +0300

    first commit

---
+++ gulpfile.js
@@ -0,0 +1,65 @@
+const {src, dest, parallel, series, watch} = require('gulp');
+const autoPrefixer = require('gulp-autoprefixer');
+const sass = require('gulp-sass')(require('sass'));
+const notify = require('gulp-notify');
+const rename = require('gulp-rename');
+const autoprefixer = require('gulp-autoprefixer');
+const cleanCSS = require('gulp-clean-css');
+const sourcemaps = require('gulp-sourcemaps');
+const browserSync = require('browser-sync').create();
+const fileinclude = require('gulp-file-include');
+
+
+const styles = () => {
+return src('./src/scss/**/*.scss')
+.pipe(sourcemaps.init())
+.pipe(sass({
+  outputStyle :'expanded'
+}).on('error', notify.onError()))
+.pipe(rename({
+  suffix: '.min'
+}))
+.pipe(autoprefixer({
+  cascade: false,
+}))
+.pipe(cleanCSS({
+  level: 2
+}))
+.pipe(sourcemaps.write('.'))
+.pipe(dest('./app/css/'))
+.pipe(browserSync.stream());
+}
+
+const htmlInclude = () => {
+  return src(['./src/index.html'])
+  .pipe(fileinclude({
+    prefix: '@',
+    basepath: '@file'
+  }))
+  .pipe(dest('./app'))
+  .pipe(browserSync.stream())
+}
+
+// const htmlInclude = () => {
+//   return src('./src/index.html')
+//   .pipe(fileinclude({
+//     prefix: '@',
+//     pasepath: '@file'
+//   }))
+//   .pipe(dest('./app'))
+//   .pipe(browserSync.stream());
+// }
+
+const watchFiles = () =>{
+  browserSync.init({
+    server: {
+      baseDir: './app'
+    }
+  });
+  watch('./src/scss/**/*.scss', styles);
+  watch('./src/index.html', htmlInclude);
+}
+
+
+exports.styles = styles;
+exports.watchFiles = watchFiles;
